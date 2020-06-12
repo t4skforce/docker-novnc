c622295828ac210735ede040fc9c519a4ec8ec49
t4skforce <t4skforce1337@gmail.com>
2020-06-13 01:18:48 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM golang:1.14-buster AS easy-novnc-build
+WORKDIR /src
+RUN set -xe && \
+    go mod init build && \
+    go get github.com/geek1011/easy-novnc@v1.1.0 && \
+    go build -o /bin/easy-novnc github.com/geek1011/easy-novnc
+
+FROM debian:buster-slim
+
+ENV HOME=/data \
+    PUID=1000 \
+    PGID=1000 \
+    HTTP_PORT=80 \
+    HTTPS_PORT=443
+
+RUN set -xe && \
+    apt-get update -y && \
+    mkdir -p /usr/share/desktop-directories && \
+    apt-get install -y --no-install-recommends \
+      openbox obconf tint2 feh papirus-icon-theme arc-theme \
+      tigervnc-standalone-server supervisor cron python3-jinja2 python3-click \
+      terminator nano wget curl openssh-client rsync ca-certificates xdg-utils htop tar xzip gzip bzip2 zip unzip fonts-dejavu fonts-liberation2 \
+      nginx-light gettext-base apache2-utils && \
+    rm -rf /etc/nginx/nginx.conf && \
+    rm -rf /etc/xdg/autostart/* /etc/xdg/openbox/* && \
+    rm -rf /var/lib/apt/lists
+
+COPY --from=easy-novnc-build /bin/easy-novnc /usr/local/bin/
+COPY ./templates/. /
+RUN set -xe && \
+    chmod 0644 /etc/cron.d/*.j2 /etc/nginx/*.j2 /etc/xdg/openbox/*.j2 /etc/*.j2 && \
+    groupadd --gid ${PUID} app && \
+    useradd --home-dir ${HOME} --shell /bin/bash --uid ${PUID} --gid ${PUID} app && \
+    mkdir -p ${HOME}
+
+WORKDIR ${HOME}
+VOLUME ${HOME}
+
+EXPOSE ${HTTP_PORT} ${HTTPS_PORT}
+ENTRYPOINT [ "docker-entrypoint.sh" ]
+CMD []
---
+++ templates/usr/bin/docker-entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+groupmod -o -g "$PGID" app
+usermod -o -u "$PUID" app
+
+# check permissions
+if [ ! "$(stat -c %u $HOME)" = "$PUID" ]; then
+    echo "Change in ownership detected, please be patient while we chown existing files"
+  	echo "This could take some time"
+  	chown app:app -R $HOME
+fi
+if [ ! "$(stat -c %u /dev/stdout)" = "$PUID" ]; then
+  chown app:app /dev/stdout
+fi
+
+
+# setup cert
+export SERVER_NAME=${SERVER_NAME-$HOSTNAME}
+if [ ! -d "$HOME/.cert/server.key" ]; then
+  echo "creating self signed certificate $HOME/.cert/nginx.crt"
+  mkdir -p $HOME/.cert
+  openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 \
+    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=${SERVER_NAME}" \
+    -keyout "$HOME/.cert/server.key" -out "$HOME/.cert/server.crt"
+fi
+# add basic auth ir requests
+if [ ! -z "$APP_USERNAME" ]; then
+  if [ ! -f "$HOME/.htpasswd" ]; then
+    touch "$HOME/.htpasswd"
+  fi
+  htpasswd -db "$HOME/.htpasswd" $APP_USERNAME $APP_PASSWORD
+fi
+# render templates
+template-render env /etc/xdg/openbox/environment.j2 /etc/nginx/nginx.conf.j2
+template-render desktop --no-visibility /usr/share/applications /etc/supervisord.conf.j2 /etc/cron.d/cronjobs.j2
+template-render desktop --no-visibility /etc/xdg/autostart /etc/xdg/openbox/autostart.j2
+template-render desktop /usr/share/applications /etc/xdg/openbox/menu.xml.j2 /etc/xdg/openbox/rc.xml.j2 /etc/tint2rc.j2 && \
+mkdir -p $HOME/.config/tint2 && \
+cp /etc/tint2rc $HOME/.config/tint2/tint2rc
+
+echo "
+-------------------------------------
+User uid:    $(id -u app)
+User gid:    $(id -g app)
+-------------------------------------
+"
+
+exec supervisord "$@"
