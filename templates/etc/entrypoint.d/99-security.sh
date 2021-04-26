e653bd21162279dda32a576299d889a611223075
t4skforce <t4skforce1337@gmail.com>
2021-04-26 08:15:56 +0200

    Update 99-security.sh
    * bugfix for "affinity:container" env variable

--- templates/etc/entrypoint.d/99-security.sh
+++ templates/etc/entrypoint.d/99-security.sh
@@ -10,12 +10,15 @@ chmod 0444 /usr/share/applications/* /etc/xdg/autostart/*
 # remove all env variables
 while IFS='=' read -r key value ; do
   [[ $key == "PATH" ]] || [[ $key == "HOSTNAME" ]] || [[ $key == "PWD" ]] || [[ $key == "HOME" ]] && continue
-  unset "$key"
+  key=$(printf "%q" "$key") # escape string
+  unset "$key" || true
 done < <(env)
 
 # load clean env variables
 while IFS='=' read -r key value ; do
   [[ $key =~ ^#.* ]] && continue
+  key=$(printf "%q" "$key") # escape string
+  value=$(printf "%q" "$value") # escape string
   export "$key=$value"
 done < "/etc/environment"
 
